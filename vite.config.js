a2d508b1dcdd80cfa71f7efa61da5511afe0f888
kingtyrant69 <aman.agarwal.1509@gmail.com>
2024-10-20 20:41:36 +0530

    changed api

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://testimonials-backend-rfra.onrender.com'
+      '/api':'https://testimonials-backend-onui.onrender.com'
     },
   },
   plugins: [react()],
